320ed8e61ec2c807e6ed8e212f332f9e19c3a6dd
NicolasLopezSTC <gclan149@gmail.com>
2023-09-17 02:46:45 -0500

    Near the end
    Signed-off-by: NicolasLopezSTC <gclan149@gmail.com>

---
+++ src/components/Main.vue
@@ -0,0 +1,20 @@
+<script setup>
+import MainSearch from './MainSearch.vue'
+import MainCards from './MainCards.vue'
+import MainPagination from './MainPagination.vue'
+</script>
+
+<template>
+    <div class="wrapper">
+    <MainSearch />
+     <MainCards />
+     <MainPagination />
+   </div>
+</template>
+
+<style scoped lang="postcss">
+.wrapper{
+@apply flex flex-col container mx-auto py-4 gap-4;
+}
+
+</style>
---
+++ src/components/MainPagination.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref } from 'vue'
+const pages = ref(10)
+const activePage = ref(4)
+
+const prevPage = () => {
+    if (activePage.value > 1) {
+        activePage.value--
+    }
+}
+
+const nextPage = () => {
+    if (activePage.value < pages.value) {
+        activePage.value++
+    }
+}
+</script>
+
+
+
+
+<template>
+  
+</template>
