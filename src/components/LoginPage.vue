9a54feb9828b20bf5720b758f5253dcbbdf824be
NicolasLopezSTC <gclan149@gmail.com>
2023-09-25 23:23:15 -0500

    nothings working
    Signed-off-by: NicolasLopezSTC <gclan149@gmail.com>

---
+++ src/components/LoginPage.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { useAuth } from '@/composables/useAuth'
+const { login } = useAuth()
+</script>
+
+
+<template>
+    <h1>Login Page</h1>
+    <button class="bg-green-400 px-4 py-4" @click="login">Log In</button>
+</template>
